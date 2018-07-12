fa4963dc4cafb8ded33fc64ae1506094ffd61b59
Dan Kogai <dankogai+github@gmail.com>
2018-07-12 13:41:19 +0900

    version 2.4.6

--- base64.js
+++ base64.js
@@ -21,7 +21,7 @@
     'use strict';
     // existing version for noConflict()
     var _Base64 = global.Base64;
-    var version = "2.4.5";
+    var version = "2.4.6";
     // if node.js, we use Buffer
     var buffer;
     if (typeof module !== 'undefined' && module.exports) {
