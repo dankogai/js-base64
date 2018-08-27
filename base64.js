d82f75345a6a4a2b0b778059f1c193706b22a8e5
Dan Kogai <dankogai+github@gmail.com>
2018-08-28 02:37:42 +0900

    version 2.4.9

--- base64.js
+++ base64.js
@@ -21,7 +21,7 @@
     'use strict';
     // existing version for noConflict()
     var _Base64 = global.Base64;
-    var version = "2.4.8";
+    var version = "2.4.9";
     // if node.js and NOT React Native, we use Buffer
     var buffer;
     if (typeof module !== 'undefined' && module.exports) {
