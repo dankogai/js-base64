05d56036d9fc86088b435ea30d4c7772b79f8cb5
Dan Kogai <dankogai+github@gmail.com>
2020-07-17 00:17:46 +0900

    version 3.1.2

--- base64.mjs
+++ base64.mjs
@@ -9,7 +9,7 @@
  * 
  * @author Dan Kogai (https://github.com/dankogai)
  */      
-const version = '3.1.1';
+const version = '3.1.2';
 const _b64chars
       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 const _b64tab = ((bin) => {
