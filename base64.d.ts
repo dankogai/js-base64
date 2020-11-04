ad8897c84bf6b2607a718fbf390ddde63cb4ceff
Dan Kogai <dankogai+github@gmail.com>
2020-11-04 09:29:16 +0900

    version 3.6.0

--- base64.d.ts
+++ base64.d.ts
@@ -9,11 +9,11 @@
  *
  * @author Dan Kogai (https://github.com/dankogai)
  */
-declare const version = "3.5.2";
+declare const version = "3.6.0";
 /**
  * @deprecated use lowercase `version`.
  */
-declare const VERSION = "3.5.2";
+declare const VERSION = "3.6.0";
 /**
  * polyfill version of `btoa`
  */
