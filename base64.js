53644d0db19d15b035a32a63f479664e86a33b5b
Dan Kogai <dankogai+github@gmail.com>
2024-01-20 13:21:41 +0900

    version 3.7.6

--- base64.js
+++ base64.js
@@ -37,7 +37,7 @@
      *
      * @author Dan Kogai (https://github.com/dankogai)
      */
-    var version = '3.7.5';
+    var version = '3.7.6';
     /**
      * @deprecated use lowercase `version`.
      */
--- base64.mjs
+++ base64.mjs
@@ -9,7 +9,7 @@
  *
  * @author Dan Kogai (https://github.com/dankogai)
  */
-const version = '3.7.5';
+const version = '3.7.6';
 /**
  * @deprecated use lowercase `version`.
  */
