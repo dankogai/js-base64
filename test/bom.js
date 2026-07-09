95ef749814426ba6fff150a90b00730b7dcfbd59
Dan Kogai <dankogai+github@gmail.com>
2026-07-09 16:01:27 +0900

    update browser test

--- test/bom.js
+++ test/bom.js
@@ -9,7 +9,7 @@ var assert = assert || require("assert");
 
 // Load Base64 in a browser-faithful environment: no Buffer, no atob/btoa,
 // but TextDecoder/TextEncoder present, so the TextDecoder decode path is taken.
-var Base64 = (function () {
+var Base64 = typeof global !== 'undefined' ? (function () {
     var savedBuffer = global.Buffer;
     var savedAtob = global.atob;
     var savedBtoa = global.btoa;
@@ -23,7 +23,7 @@ var Base64 = (function () {
     global.atob = savedAtob;
     global.btoa = savedBtoa;
     return B64;
-})();
+})() : Base64 || require('../base64.js').Base64;
 
 var is = function (a, e, m) {
     return function () {
